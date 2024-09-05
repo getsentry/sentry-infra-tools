dde0712abfa985b4d334cc7d00809384b782bf55
Nikhar Saxena <nikhar.saxena@sentry.io>
2024-09-05 10:06:50 -0700

    add libsentrykube macros

--- setup.py
+++ setup.py
@@ -7,6 +7,28 @@ macros = [
     "iap_service=sentry_kube.ext:IAPService",
     "pgbouncer_sidecar=sentry_kube.ext:PGBouncerSidecar",
     "xds_configmap_from=sentry_kube.ext:XDSConfigMapFrom",
+    "raise=libsentrykube.ext:RaiseExtension",
+    "json_file=libsentrykube.ext:JsonFile",
+    "values_of=libsentrykube.ext:ValuesOf",
+    "deployment_image=libsentrykube.ext:DeploymentImage",
+    "statefulset_image=libsentrykube.ext:StatefulSetImage",
+    "machine_info=libsentrykube.ext:MachineType",
+    "md5file=libsentrykube.ext:Md5File",
+    "md5template=libsentrykube.ext:Md5Template",
+    "ha_pod_affinity=libsentrykube.ext:HAPodAffinity",
+    "internal_service=libsentrykube.ext:InternalService",
+    "envoy_sidecar=libsentrykube.ext:EnvoySidecar",
+    "dogstatsd_port_forward_initcontainer=libsentrykube.ext:DogstatsdPortForwardingInitContainer",  # noqa: E501
+    "geoip_volume=libsentrykube.ext:GeoIPVolume",
+    "geoip_volumemount=libsentrykube.ext:GeoIPVolumeMount",
+    "geoip_initcontainer=libsentrykube.ext:GeoIPInitContainer",
+    "serviceaccount=libsentrykube.ext:ServiceAccount",
+    "deep_merge=libsentrykube.ext:DeepMerge",
+    "sysctl_initcontainer=libsentrykube.ext:SysctlInitContainer",
+    "xds_eds_cluster_config=libsentrykube.ext:XDSEDSClusterConfig",
+    "xds_proxy_sidecar=libsentrykube.ext:XDSProxySidecar",
+    "xds_proxy_initcontainer=libsentrykube.ext:XDSProxyInitContainer",
+    "xds_proxy_volume=libsentrykube.ext:XDSProxyVolume",
 ]
 
 
