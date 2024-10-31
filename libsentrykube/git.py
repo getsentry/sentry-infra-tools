feda50152db306e6d36e17ddf6de9a45bc81135a
Nikhar Saxena <nikhar.saxena@sentry.io>
2024-10-31 14:48:01 -0700

    add switch_to_branch

--- libsentrykube/git.py
+++ libsentrykube/git.py
@@ -60,26 +60,3 @@ class Git:
     def switch_to_branch(self, branch_name: str) -> None:
         branch = next(head for head in self.repo.heads if head.name == branch_name)
         branch.checkout()
-
-
-def go_to_main() -> None:
-    """
-    Switches to main stashes the current work.
-    TODO: Add arguments
-    """
-    raise NotImplementedError
-
-
-def pull_main() -> None:
-    """
-    Pull main
-    TODO: Add arguments
-    """
-    raise NotImplementedError
-
-
-def create_branch() -> None:
-    """
-    TODO: Add arguments
-    """
-    raise NotImplementedError
