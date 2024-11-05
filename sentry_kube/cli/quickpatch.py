dd6f886696cb843f7bda378b4f9ac87878e784dc
Filippo Pacifici <fpacifici@sentry.io>
2024-11-04 16:21:22 -0800

    Wire up patches into sentry-kube quickpatch (#52)
    Wire up what we built.
    
    Specifically:
    1. create three subcommands (they are arguments, not subcommands). One
    for render, diff and apply
    2. Implement diff and render. Tested against s4s and it works 
    3. Added a rudimentary parsing of arguments from the command line
    3. Fixed the quickpatch module in case the managed file is not there.
    Now it gets created
    4. Tested against a test patch in snuba s4s.
    
    What is missing (separate PRs)
    - validation of the arguments according to the types defined in the
    jsonschema in the patch
    - figure out how to express a patch that is able to either update fields
    or create paths
    - implement apply 
    - add the git part

--- sentry_kube/cli/quickpatch.py
+++ sentry_kube/cli/quickpatch.py
@@ -4,7 +4,8 @@ import click
 from libsentrykube.git import Git
 from libsentrykube.quickpatch import apply_patch, get_arguments
 from libsentrykube.kube import render_templates
-from typing import MutableMapping, Sequence
+from sentry_kube.cli.apply import _diff_kubectl
+from typing import Sequence, MutableMapping
 
 from libsentrykube.utils import workspace_root
 
@@ -30,9 +31,14 @@ __all__ = ("quickpatch",)
     default=False,
     help="Force the creation of the new branch.",
 )
+# We cannot make `quickpatch` a group and render/diff/apply sub-commands as we should
+# because sentry-kube does not store a dictionary in click.ctx. It uses a data class
+# which is the same for all commands, so we cannot easily customize it for a specific
+# command.
+@click.argument("action", type=click.Choice(["render", "diff", "apply"]))
 @click.pass_context
 def quickpatch(
-    ctx: click.core.Context,
+    ctx: click.Context,
     service: str,
     resource: str,
     patch: str,
@@ -40,6 +46,7 @@ def quickpatch(
     no_pull: bool,
     no_pr: bool,
     force_branch_creation: bool,
+    action: str,
 ):
     """
     Applies pre-defined patches to the value files of our Kubernetes services.
@@ -65,8 +72,12 @@ def quickpatch(
         git.merge_origin(git.default_branch)
 
     get_arguments(service, patch)
+
     # TODO: Validate all arguments are passed and prompt for the missing ones.
     populated_arguments: MutableMapping[str, str] = {}
+    for arg in arguments:
+        key, value = arg.split("=", 2)
+        populated_arguments[key] = value
     apply_patch(
         service,
         ctx.obj.customer_name,
@@ -76,12 +87,32 @@ def quickpatch(
         cluster=ctx.obj.cluster_name,
     )
 
-    render_templates(
-        ctx.obj.customer_name,
-        service,
-        ctx.obj.cluster_name,
-        filters=[f"metadata.name={resource}"],
-    )
+    if action == "render":
+        print(
+            render_templates(
+                ctx.obj.customer_name,
+                service,
+                ctx.obj.cluster_name,
+                filters=[f"metadata.name={resource}"],
+            )
+        )
+    elif action == "diff":
+        definitions = "".join(
+            render_templates(
+                ctx.obj.customer_name,
+                service,
+                ctx.obj.cluster_name,
+                filters=[f"metadata.name={resource}"],
+            )
+        ).encode("utf-8")
+        return _diff_kubectl(
+            ctx=ctx,
+            definitions=definitions,
+        )
+    elif action == "apply":
+        raise NotImplementedError("Apply is not implemented yet")
+    else:
+        raise ValueError(f"Invalid action {action}")
 
     # Clean up the branch
     # TODO: At some point we should make this a context manager so cleanup
@@ -108,5 +139,4 @@ def quickpatch(
         git.switch_to_branch(git.previous_branch)
         git.pop_stash()
 
-    # TODO: Apply to prod
     # TODO: File PR
