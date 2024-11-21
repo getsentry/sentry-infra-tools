8c1aecfafaa10f87871fe56ebc24bd16bde9d980
Martin Linzmayer <martin.linzmayer@sentry.io>
2024-11-21 11:53:49 +0100

    test: add tests for new overrides

--- libsentrykube/tests/conftest.py
+++ libsentrykube/tests/conftest.py
@@ -79,6 +79,26 @@ CONFIGURATION = {
     },
 }
 
+COMMON_SHARED_CONFIG = {
+    "config": {
+        "foo": "123",
+        "baz": "test",
+        "settings": {
+            "abc": 10,
+            "def": "test"
+        }
+    }
+}
+
+CLUSTER_OVERRIDE_CONFIG = {
+    "config": {
+        "foo": "not-foo",
+        "settings": {
+            "abc": 20
+        }
+    }
+}
+
 
 @pytest.fixture
 def config_structure() -> Generator[str, None, None]:
@@ -109,6 +129,74 @@ def config_structure() -> Generator[str, None, None]:
 
         os.makedirs(Path(temp_dir) / "cli_config")
         with open(Path(temp_dir) / "cli_config/configuration.yaml", "w") as f:
+            f.write(safe_dump(CONFIGURATION))
+
+        yield temp_dir
+
+@pytest.fixture
+def hierarchical_override_structure() -> Generator[str, None, None]:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        k8s = Path(temp_dir) / "k8s"
+        os.makedirs(k8s / "clusters")
+        clusters = k8s / "clusters"
+        with open(clusters / "cluster1.yaml", "w") as f:
+            f.write(safe_dump(CLUSTER_1))
+        with open(clusters / "cluster2.yaml", "w") as f:
+            f.write(safe_dump(CLUSTER_2))
+
+        service_dir = k8s / "services" / "my_service"
+        region_overrides = service_dir / "region_overrides"
+        os.makedirs(region_overrides)
+        
+        os.makedirs(k8s / "services" / "another_service")
+        common_config = region_overrides / "common_shared_config"
+        os.makedirs(common_config)
+        
+        with open(common_config / "_values.yaml", "w") as f:
+            f.write(safe_dump(COMMON_SHARED_CONFIG))
+
+        region_dir = common_config / "customer1"
+        os.makedirs(region_dir)
+
+        with open(region_dir / "cluster1.yaml", "w") as f:
+            f.write(safe_dump(CLUSTER_OVERRIDE_CONFIG))
+
+        os.makedirs(Path(temp_dir) / "cli_config")
+        with open(Path(temp_dir) / "cli_config" / "configuration.yaml", "w") as f:
+            f.write(safe_dump(CONFIGURATION))
+
+        os.environ["SENTRY_KUBE_CONFIG_FILE"] = str(Path(temp_dir) / "cli_config" / "configuration.yaml")
+
+        yield temp_dir
+
+@pytest.fixture
+def regional_cluster_specific_override_structure() -> Generator[str, None, None]:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        k8s = Path(temp_dir) / "k8s"
+        os.makedirs(k8s / "clusters")
+        clusters = k8s / "clusters"
+        with open(clusters / "cluster1.yaml", "w") as f:
+            f.write(safe_dump(CLUSTER_1))
+        with open(clusters / "cluster2.yaml", "w") as f:
+            f.write(safe_dump(CLUSTER_2))
+
+        service_dir = k8s / "services" / "my_service"
+        region_overrides = service_dir / "region_overrides"
+        os.makedirs(region_overrides)
+
+        os.makedirs(k8s / "services" / "another_service")
+
+        region_dir = region_overrides / "customer1"
+        os.makedirs(region_dir)
+
+        with open(region_dir / "_values.yaml", "w") as f:
+            f.write(safe_dump(COMMON_SHARED_CONFIG))
+
+        with open(region_dir / "cluster1.yaml", "w") as f:
+            f.write(safe_dump(CLUSTER_OVERRIDE_CONFIG))
+
+        os.makedirs(Path(temp_dir) / "cli_config")
+        with open(Path(temp_dir) / "cli_config" / "configuration.yaml", "w") as f:
             f.write(safe_dump(CONFIGURATION))
 
         yield temp_dir
