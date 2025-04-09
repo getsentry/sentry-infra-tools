5d7a510b98eb67d085c3b482ad61ad3b633a3999
Ben McKerry <110857332+bmckerry@users.noreply.github.com>
2025-04-09 17:28:04 -0400

    don't check if external enabled

--- libsentrykube/iap.py
+++ libsentrykube/iap.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 
 import click
 import yaml
@@ -10,38 +9,8 @@ KUBE_CONFIG_PATH = os.getenv(
     os.path.expanduser("~/.kube/config"),
 )
 
-KUBECTL_JUMP_HOST = os.getenv(
-    "SENTRY_KUBE_KUBECTL_JUMP_HOST",
-    "scratch",
-)
 
-
-def _is_external_connection_allowed(
-    project: str, region: str, cluster_name: str
-) -> bool:
-    """
-    Check if external connections are enabled for the cluster.
-    """
-    is_enabled = subprocess.check_output(
-        [
-            "gcloud",
-            "container",
-            "clusters",
-            "describe",
-            cluster_name,
-            "--region",
-            region,
-            "--project",
-            project,
-            "--format",
-            "value(controlPlaneEndpointsConfig.dnsEndpointConfig.allowExternalTraffic)",
-        ],
-    )
-    res = is_enabled.decode("utf-8").strip()
-    return True if res == "True" else False
-
-
-def ensure_iap_tunnel(ctx: click.core.Context, quiet: bool = False) -> str:
+def ensure_iap_tunnel(ctx: click.core.Context) -> str:
     """
     Create an IAP tunnel and generate a temporary kubeconfig that uses it.
 
@@ -50,7 +19,6 @@ def ensure_iap_tunnel(ctx: click.core.Context, quiet: bool = False) -> str:
     """
     port: int = ctx.obj.cluster.services_data["iap_local_port"]
     context = ctx.obj.context_name
-    _, project, region, cluster = context.split("_")
 
     with open(KUBE_CONFIG_PATH) as kubeconfig_file:
         kubeconfig = yaml.safe_load(kubeconfig_file)
@@ -60,15 +28,11 @@ def ensure_iap_tunnel(ctx: click.core.Context, quiet: bool = False) -> str:
                 break
         else:
             # example: gke_internal-sentry_us-central1-b_zdpwkxst
+            _, project, region, cluster = context.split("_")
             raise ValueError(
                 f"Can't find k8s cluster for {context} context. You might need to run:\n"
                 f"gcloud container clusters get-credentials {cluster} "
                 f"--region {region} --project {project}"
-            )
-        if not _is_external_connection_allowed(project, region, cluster):
-            raise ValueError(
-                f"External connections are not allowed for cluster {cluster} in"
-                f"region {region}, project {project}."
             )
 
         tmp_kubeconfig_path = os.path.join(
