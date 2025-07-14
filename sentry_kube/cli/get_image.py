28e7a4a0882cc8318f453b3b66621f492ca7e1ba
Michael Warkentin <michael@sentry.io>
2025-07-14 12:23:55 -0400

    PRODENG-227: add service -> deployment/container mapping

--- sentry_kube/cli/get_image.py
+++ sentry_kube/cli/get_image.py
@@ -4,13 +4,33 @@ from libsentrykube.service import get_service_deployment_image
 __all__ = ("get_image",)
 
 
+SERVICE_DEPLOYMENT_CONTAINER_MAP = {
+    "chartcuterie": ("chartcuterie-web-production", "chartcuterie"),
+    "getsentry": ("getsentry-web-default-common-production", "sentry"),
+    "getsentry-control": ("getsentry-control-web-default-common-production", "sentry"),
+    "relay": ("relay-default-production", "relay"),
+    "relay-pop": ("relay-pop", "relay"),
+    "release-registry": ("release-registry", "release-registry"),
+    "reload": ("reload", "reload"),
+    "script-runner": ("script-runner/script-runner-region", "script-runner-region"),
+    "seer": ("seer-web-severity", "seer"),
+    "seer-gpu": ("seer-gpu-web-group-ingest", "seer-gpu"),
+    "snuba": ("snuba-api-production", "api"),
+    "super-big-consumers": ("sbc-arroyo-errors", "super-big-consumers"),
+    "symbol-collector": ("symbol-collector", "symbol-collector"),
+    "tempest": ("tempest-default", "tempest"),
+    "vroom": ("vroom-default-production", "vroom"),
+}
+
+
 @click.command()
 @click.argument("service", type=str)
-@click.argument("container", type=str)
-def get_image(service: str, container: str) -> None:
+def get_image(service: str) -> None:
     """
     Gets the deployment image for a specific service.
     """
+    service, container = SERVICE_DEPLOYMENT_CONTAINER_MAP[service]
+
     image = get_service_deployment_image(
         service=service, container=container, default="unknown"
     )
