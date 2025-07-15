3226fb988ad58ed7500b3b1d463b76d50fdc6615
Michael Warkentin <michael@sentry.io>
2025-07-15 14:15:59 -0400

    PRODENG-227: fetch service container map from customer config
    Defined in ops here: https://github.com/getsentry/ops/pull/16219

--- sentry_kube/cli/get_image.py
+++ sentry_kube/cli/get_image.py
@@ -1,26 +1,10 @@
 import click
+
+from libsentrykube.config import Config
+from libsentrykube.customer import get_service_container_map
 from libsentrykube.service import get_deployment_image
 
 __all__ = ("get_image",)
-
-
-SERVICE_DEPLOYMENT_CONTAINER_MAP = {
-    "chartcuterie": ("chartcuterie-web-production", "chartcuterie"),
-    "getsentry": ("getsentry-web-default-common-production", "sentry"),
-    "getsentry-control": ("getsentry-control-web-default-common-production", "sentry"),
-    "relay": ("relay-default-production", "relay"),
-    "relay-pop": ("relay-pop", "relay"),
-    "release-registry": ("release-registry", "release-registry"),
-    "reload": ("reload", "reload"),
-    "script-runner": ("script-runner/script-runner-region", "script-runner-region"),
-    "seer": ("seer-web-severity", "seer"),
-    "seer-gpu": ("seer-gpu-web-group-ingest", "seer-gpu"),
-    "snuba": ("snuba-api-production", "api"),
-    "super-big-consumers": ("sbc-arroyo-errors", "super-big-consumers"),
-    "symbol-collector": ("symbol-collector", "symbol-collector"),
-    "tempest": ("tempest-default", "tempest"),
-    "vroom": ("vroom-default-production", "vroom"),
-}
 
 
 @click.command()
@@ -30,12 +14,18 @@ def get_image(ctx, service: str) -> None:
     """
     Gets the deployment image for a specific service.
     """
-    deployment, container = SERVICE_DEPLOYMENT_CONTAINER_MAP[service]
+
+    config = Config()
+    service_container_map = get_service_container_map(
+        config, ctx.obj.customer_name, ctx.obj.cluster_name
+    )
+    service_map = service_container_map[service]
 
     image = get_deployment_image(
-        deployment=deployment,
-        container=container,
+        deployment=service_map["deployment"],
+        container=service_map["container"],
         default="unknown",
         quiet=ctx.obj.quiet_mode,
     )
+
     click.echo(image)
