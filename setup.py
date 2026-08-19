9286f5ab41884696072116c862f61959bd84e0c2
dmajere <4362854+dmajere@users.noreply.github.com>
2026-08-19 00:09:01 +0000

    release: 1.32.3

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def get_requirements() -> Sequence[str]:
 
 setup(
     name="sentry-infra-tools",
-    version="1.32.1",
+    version="1.32.3",
     author="Sentry",
     author_email="oss@sentry.io",
     packages=find_packages(where=".", exclude="tests"),
